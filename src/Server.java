ce945cdc297a7a1f2144393a4b17271605bba924
name <tzolcubekov@gmail.com>
2025-11-15 14:05:46 +0600

    Refactored Server class, now only establishes connection with client

--- src/Server.java
+++ src/Server.java
@@ -4,15 +4,17 @@ import java.io.PrintWriter;
 import java.io.Writer;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Server {
-    private final int port;;
-
+    private final int port;
     private final ExecutorService pool = Executors.newCachedThreadPool();
+    private final List<ClientHandler> clients = new CopyOnWriteArrayList<>();
 
     public Server(int port) {
         this.port = port;
@@ -24,60 +26,15 @@ public class Server {
 
     public void run() {
         try(ServerSocket server = new ServerSocket(port)){
+            System.out.println("Server started on port: " + port);
             while(!server.isClosed()){
                 Socket socket = server.accept();
-                pool.submit(() -> handle(socket));
+                ClientHandler clientHandler = new ClientHandler(socket, clients);
+                clients.add(clientHandler);
             }
         } catch (IOException e) {
             System.out.format("Вероятнее всего порт %s занят%n", port);
             e.printStackTrace();
         }
-    }
-
-    private void handle(Socket socket) {
-        System.out.format("Connected client: %s%n", socket);
-
-        try(
-                socket;
-                Scanner reader = getReader(socket);
-                PrintWriter writer = getWriter(socket);
-                ) {
-            sendResponse("hello" + socket, writer);
-            while (true){
-                String message = reader.nextLine().trim();
-                if(isEmptyMsg(message) || isQuitMsg(message)){
-                    break;
-                }
-                sendResponse(message.toUpperCase(), writer);
-                System.out.format("Got message: %s%n", message);
-            }
-        } catch (NoSuchElementException e) {
-            System.out.println("Client dropped connection");
-        } catch (IOException e){
-            e.printStackTrace();
-        }
-        System.out.println("Client disconnected");
-    }
-
-    private PrintWriter getWriter(Socket socket) throws IOException {
-        return new PrintWriter(socket.getOutputStream());
-    }
-
-    private Scanner getReader(Socket socket) throws IOException {
-        return new Scanner(new InputStreamReader(socket.getInputStream()));
-    }
-
-    private static boolean isQuitMsg(String msg) {
-        return "bye".equalsIgnoreCase(msg);
-    }
-
-    private static boolean isEmptyMsg(String msg) {
-        return msg == null || msg.isBlank();
-    }
-
-    private static void sendResponse(String response, Writer writer) throws IOException {
-        writer.write(response);
-        writer.write(System.lineSeparator());
-        writer.flush();
     }
 }
